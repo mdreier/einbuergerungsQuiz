120e8158866d717aa24ad9374a88889fa348a093
Martin Dreier <martin@martindreier.de>
2019-01-24 23:46:11 +0100

    Add start page

--- webapp/Component.js
+++ webapp/Component.js
@@ -4,11 +4,16 @@ sap.ui.define([
     "use strict";
     return UIComponent.extend("de.martindreier.ebq.Component", {
 
-        manifest: "json",
+        metadata: {
+            manifest: "json"
+        },
 
         init: function () {
             // call the init function of the parent
             UIComponent.prototype.init.apply(this, arguments);
+
+            //Initialize routing
+            this.getRouter().initialize();
         }
     });
 });
