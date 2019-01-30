1f130cba3432443a494525a8315a170f1474f170
Martin Dreier <martin@martindreier.de>
2019-01-30 17:10:39 +0100

    Handle correct/incorrect answers

--- webapp/controller/Play.controller.js
+++ webapp/controller/Play.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
     "de/martindreier/ebq/controller/BaseController",
-    "sap/ui/model/json/JSONModel"
- ], function (BaseController, JSONModel) {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/ButtonType"
+ ], function (BaseController, JSONModel, ButtonType) {
     "use strict";
     return BaseController.extend("de.martindreier.ebq.controller.Play", {
         
@@ -9,6 +10,31 @@ sap.ui.define([
             this._quiz = this.getComponent().getQuiz();
             this._quiz.getModel().attachPropertyChange(this._onQuestionUpdate, this);
             this.getRouter().getRoute("play").attachPatternMatched(this._onShowCurrentQuestion, this);
+            
+            let oModel = new JSONModel({
+                resultsActive: false
+            });
+            this.getView().setModel(oModel, "view");
+        },
+
+        onAnswer: function(oEvent)
+        {
+            let oButton = oEvent.getSource();
+            let bCorrect = oButton.getBindingContext().getProperty("correct");
+            if (bCorrect)
+            {
+                oButton.setType(ButtonType.Accept);
+            } else {
+                oButton.setType(ButtonType.Reject);
+            }
+            this.getView().getModel("view").setProperty("/resultsActive", true);
+        },
+
+        onNextQuestion: function()
+        {
+            this.getView().getModel("view").setProperty("/resultsActive", false);
+            this._resetButtons();
+            //TODO: Load next question
         },
 
         _onQuestionUpdate: function(oEvent)
@@ -36,6 +62,14 @@ sap.ui.define([
             let sBindingPath = "/" + oQuestion.set + "/" + oQuestion.index + "/";
             this.getView().bindElement(sBindingPath);
             //TODO: Handle invalid question
+        },
+
+        _resetButtons: function()
+        {
+            for (let oButton of this.getView().byId("questionsSection").getItems())
+            {
+                oButton.setType(ButtonType.Default);
+            }
         }
 
     });
